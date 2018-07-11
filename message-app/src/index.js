fc2afb39f7604fcb92f4532a580c4e781b02bc3c
Tasneem Farag <tasneem.a.farag@gmail.com>
2018-07-10 22:33:00 -0400

    adding what I have for now

---
+++ message-app/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const user = {
+  firstName: "Jane",
+  lastName: "Doe"
+}
+const element = <p>Hello, {user.firstName} {user.lastName}</p>
+
+ReactDOM.render(element, document.getElementById('root'));
